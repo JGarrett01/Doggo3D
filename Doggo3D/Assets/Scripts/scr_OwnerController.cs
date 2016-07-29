3209aa99f2b12829c7a6e0089080fc71838a50a2
Jake Garrett <jake-garrett@hotmail.com>
2016-07-29 14:58:43 +1000

    hey thats pretty good

---
+++ Doggo3D/Assets/Scripts/scr_OwnerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class scr_OwnerController : MonoBehaviour {
+
+	public float thrust;
+	public Rigidbody rb;
+	public List<GameObject> spheres;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		FindAllSpheres ();
+		Debug.Log(spheres.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			rb.AddForce(transform.forward * thrust);
+		}
+	}
+
+	void FindAllSpheres () {
+		GameObject[] sphoreos = UnityEngine.Object.FindObjectsOfType<GameObject>() ;
+		for(var i = 0; i < sphoreos.Length; ++i)
+		{
+			if(sphoreos[i].name == "Sphere")
+			{
+				spheres.Add (sphoreos [i]);
+			}
+		}
+	}
+}
---
+++ Doggo3D/Assets/Scripts/scr_PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_PlayerController : MonoBehaviour {
+
+	public bool canPickup = true;
+	public bool hasPickup = false;
+	public GameObject mouthHole;
+	public GameObject currPickup;
+	public GameObject player;
+	public CapsuleCollider capCol;
+	public CharacterJoint joint;
+
+
+	void Start () 
+	{
+		capCol = GetComponent<CapsuleCollider> ();
+		joint = GetComponentInChildren<CharacterJoint> ();
+	}
+
+	void LateUpdate () 
+	{
+	//	currPickup.GetComponent<Collider> ().enabled = true;
+		PickupController ();
+
+
+	}
+
+
+	//Pickup Controller decided whether an object can be picked up
+	void PickupController()
+	{
+		if (currPickup != null && Input.GetAxis ("RightTrigger") >= 0.2) 
+		{
+			currPickup.transform.position = mouthHole.transform.position;
+			joint.connectedBody = currPickup.GetComponent<Rigidbody>() ;
+
+//			if (!currPickup.transform.IsChildOf(player.transform))
+//				{
+//					currPickup.transform.parent = player.transform;
+//				}
+
+			currPickup.GetComponent<Collider> ().enabled = false;
+
+			capCol.enabled = false;
+		}
+
+
+
+		if (currPickup != null && Input.GetAxis ("RightTrigger") < 0.2) 
+		{
+			//currPickup.transform.parent = null;
+			currPickup.GetComponent<Collider> ().enabled = true;
+			currPickup = null;
+			capCol.enabled = true;
+			joint.connectedBody = null;
+		}
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (currPickup == null) 
+		{
+			if (other.gameObject.tag == "Pickup")
+			{
+				currPickup = other.gameObject;
+			}
+		}
+
+			
+	}
+
+//	void OnTriggerExit(Collider other)
+//	{
+//		currPickup = null;
+//	}
+}
