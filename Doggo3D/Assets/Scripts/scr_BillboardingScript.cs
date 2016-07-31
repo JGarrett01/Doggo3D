bc57e80e4be07bfbd5e4cae63822d44e65598351
Jake Andrews <jakeandrews.karatekid@gmail.com>
2016-07-31 11:00:27 +1000

    Park level built, all dog placements and placeholders in. Just need to work out a dialogue system but need the dog in and working to do so.

---
+++ Doggo3D/Assets/Scripts/scr_BillboardingScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_BillboardingScript : MonoBehaviour {
+
+    //this script just makes anything you want look at the player. Just using it temp for the dogs.
+
+        void Update()
+        {
+            transform.LookAt(Camera.main.transform.position, Vector3.up);
+        }
+    }
