10af1e4943263e117a4292bf27ac960016386f75
Jake Andrews <jakeandrews.karatekid@gmail.com>
2016-08-08 17:46:38 +1000

    Maple done

--- Doggo3D/Assets/Scripts/scr_Maple.cs
+++ Doggo3D/Assets/Scripts/scr_Maple.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Fungus;
 
 public class scr_Maple : MonoBehaviour {
+
+    public bool hasGreeted = false;
+    public float timer = 5;
+
 
 	// Use this for initialization
 	void Start () {
@@ -11,5 +16,30 @@ public class scr_Maple : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+        if(hasGreeted == true)
+        {
+
+
+            timer -= Time.deltaTime;
+
+        }
+         
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player" && hasGreeted == false)
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Maple1");
+            hasGreeted = true;
+
+        }
+
+        if (other.gameObject.tag == "Player" && hasGreeted == true && timer <= 0)
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Maple2");
+        }
+
+    }
 }
