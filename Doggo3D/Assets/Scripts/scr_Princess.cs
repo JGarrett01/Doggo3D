1e196b2a464478e7a91e47bc5fbbbcce1693c3da
Jake Andrews <jakeandrews.karatekid@gmail.com>
2016-08-08 21:56:06 +1000

    Realized a couple issues... easy fix.

--- Doggo3D/Assets/Scripts/scr_Princess.cs
+++ Doggo3D/Assets/Scripts/scr_Princess.cs
@@ -3,13 +3,61 @@ using System.Collections;
 
 public class scr_Princess : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public scr_PlayerController playerController;
+    public bool hasBone = false;
+    public GameObject inMouth;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (playerController.currPickup != null)
+        {
+            inMouth = playerController.currPickup;
+        }
+
+        if (inMouth.tag == "Pickup" && playerController.currPickup != null)
+        {
+
+            hasBone = true;
+
+        }
+
+    
+
+
+    }
+
+    void Interactions()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && hasBone == false)
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Princess");
+        }
+
+
+
+
+        if (other.gameObject.tag == "Player" && hasBone == true)
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Princess2");
+  
+
+        }
+
+
+
+
+    }
 }
