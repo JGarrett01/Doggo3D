7375f19cfb9e54f208bdfa62ed048cc581aaddd9
Jake Andrews <jakeandrews.karatekid@gmail.com>
2016-08-08 17:06:11 +1000

    Jax Complete. I got this alking thingy in the bag bb.

--- Doggo3D/Assets/Scripts/scr_Jax.cs
+++ Doggo3D/Assets/Scripts/scr_Jax.cs
@@ -7,6 +7,8 @@ public class scr_Jax : MonoBehaviour {
 	public scr_PlayerController playerController;
 	public bool hasBall = false;
 	public GameObject inMouth;
+    public bool hasCompleted = false;
+    public float timer = 5.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +22,21 @@ public class scr_Jax : MonoBehaviour {
 		{
 			inMouth = playerController.currPickup; 
 		}
+
+        if (inMouth.tag == "TennisBall" && playerController.currPickup != null)
+        {
+
+            hasBall = true;
+
+        }
+
+        if(hasCompleted == true)
+        {
+
+            timer -= Time.deltaTime;
+
+
+        }
 
 
 	}
@@ -39,9 +56,19 @@ public class scr_Jax : MonoBehaviour {
 
 
 
-		if (other.gameObject.tag == "Player" && other.gameObject.tag == "TennisBall" && hasBall == true) 
+		if (other.gameObject.tag == "Player" && hasBall == true && hasCompleted == false) 
 		{
-			Fungus.Flowchart.BroadcastFungusMessage ("JaxBall");
+			Fungus.Flowchart.BroadcastFungusMessage ("Jax");
+            hasCompleted = true;
+           
 		}
-	}
+
+
+
+        if (other.gameObject.tag == "Player" && hasCompleted == true && timer <= 0)
+        {
+            Fungus.Flowchart.BroadcastFungusMessage("Jax3");
+        }
+
+    }
 }
