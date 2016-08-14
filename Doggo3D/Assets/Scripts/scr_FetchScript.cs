e87f1f30e0bf7464d3e998761953a46574f98c65
Jake Garrett <jake-garrett@hotmail.com>
2016-08-15 00:26:57 +1000

    started making fetch

---
+++ Doggo3D/Assets/Scripts/scr_FetchScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_FetchScript : MonoBehaviour {
+
+	public Transform target;
+	public float moveSpeed = 3;
+	public float rotSpeed = 3;
+
+
+	void Start () {
+		target = GameObject.FindWithTag ("TennisBall2").transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		
+		this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (target.position - this.transform.position), rotSpeed * Time.deltaTime);
+
+		this.transform.position += this.transform.forward * moveSpeed * Time.deltaTime;
+
+	}
+		
+}
---
+++ Doggo3D/Assets/Scripts/scr_Owner2Fetch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_Owner2Fetch : MonoBehaviour {
+
+	public GameObject ballLaunch;
+	public Rigidbody rb;
+	public float thrust;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "TennisBall2")
+		{
+			rb = other.GetComponent<Rigidbody> ();
+			other.gameObject.transform.position = ballLaunch.transform.position;
+
+			rb.AddForce (transform.forward * thrust);
+		}
+	}
+}
